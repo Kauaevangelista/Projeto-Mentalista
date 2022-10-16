eb4826956a2a8bc7487e381cc4f33582afcb0df5
Kaua Evangelista <kaua13evangelista@gmail.com>
2022-10-15 23:05:33 -0300

    commit 1

---
+++ README.md
@@ -0,0 +1,20 @@
+# Semana Imersão DEV Da Alura
+
+![preview](./.github/preview.png)
+
+>Aula 3
+
+Projeto construido no Semana Imersão DEV Da Alura com toques a mais feitos por mim.
+
+[Clique aqui para acessar](https://kauaevangelista.github.io/Projeto-Mentalista/)
+
+## 🛠️Tecnologias 
+
+- HTML
+- CSS
+- Javascript
+- Git & Github
+
+## Contato 📲
+
+kaua13evangelista@gmail.com
---
+++ index.js
@@ -0,0 +1,35 @@
+var numeroSecreto = parseInt(Math.random() * 11);
+
+var chances = 3;
+
+
+function chutar() {
+
+    var resultado = document.getElementById("resultado");
+
+    var chute = parseInt(document.getElementById("valor").value);
+
+    if (chute === numeroSecreto) {
+        resultado.innerHTML = "Parabéns, Você Acertou";
+    } else if (chute > 10 || chute < 0) {
+        resultado.innerHTML = "Você deve digitar um número de 0 a 10";
+    } else if (chute > numeroSecreto) {
+        resultado.innerHTML = "Errou, o número secreto é menor que " + chute;
+    } else {
+        resultado.innerHTML = "Errou, o número secreto é maior que " + chute;
+    }
+
+
+    var chancesHtml = document.getElementById("chances");
+
+    if (chute !== numeroSecreto) {
+        chances--;
+        chancesHtml.innerHTML = "Você tem " + chances + " chances restantes";
+    } 
+
+    if (chances === 0) {
+        chancesHtml.innerHTML = "Suas tentativas acabaram, o número secreto é o " + numeroSecreto;
+    }
+
+  }
+
